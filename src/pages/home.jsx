bd262639d43f2d69539c9f8b4a0479670fb9a4ef
Thomas CHE <jordanlafond76@gmail.com>
2025-08-13 23:19:22 +0200

    Correction beug final

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Banner from '../components/banner/banner.jsx';
-import homeBanner from "../assets/home_banner.png"
-import Card from '../components/card/Card.jsx';
-import data from '../data/logements.json';
-import './home.scss';
-console.log(data);
-
+import React from "react";
+import Banner from "../components/banner/banner.jsx";
+import homeBanner from "../assets/home_banner.png";
+import Card from "../components/card/Card.jsx";
+import data from "../data/logements.json";
+import "./home.scss";
 
 function Home() {
   return (
     <div className="home">
-     <Banner image={homeBanner} text="Chez vous, partout et ailleurs" />
+      <Banner image={homeBanner} text="Chez vous, partout et ailleurs" />
       <div className="gallery">
-        {data?.map(({title,cover,id}) =><Card title={title} id={id} cover={cover}/> )} 
+        {data?.map(({ title, cover, id }) => (
+          <Card key={id} title={title} id={id} cover={cover} />
+        ))}
       </div>
-  </div>
-  )
+    </div>
+  );
 }
 export default Home;
